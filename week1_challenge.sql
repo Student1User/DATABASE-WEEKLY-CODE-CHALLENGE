2092b2f5a43ccf2d36f22482f9d954bde9953fb5
Emmanuel Jesse <magachi.emmanuel@students.jkuat.ac.ke>
2025-03-19 20:00:30 +0300

    Create week1_challenge.sql

---
+++ week1_challenge.sql
@@ -0,0 +1,8 @@
+-- Creating a Sales Database
+CREATE DATABASE SalesDB;
+
+-- Creating and Dropping a School Database
+CREATE DATABASE SchoolDB;
+
+-- Dropping the School Database as a test run
+DROP DATABASE SchoolDB;
